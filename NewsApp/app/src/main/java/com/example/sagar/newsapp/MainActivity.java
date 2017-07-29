94a1e6b19b374542275d9d9307e0b85fb7f55976
sagarvadher <sagar@sagars-MacBook-Pro.local>
2017-07-29 00:00:08 -0700

    HW4

---
+++ NewsApp/app/src/main/java/com/example/sagar/newsapp/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.sagar.newsapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.sagar.newsapp.Data.Contract;
+import com.example.sagar.newsapp.Data.DBHelper;
+import com.example.sagar.newsapp.Data.DatabaseUtils;
+import com.example.sagar.newsapp.Utilities.RefreshNews;
+import com.example.sagar.newsapp.Utilities.ScheduleUtilities;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends  AppCompatActivity implements LoaderManager.LoaderCallbacks<Void>,
+        NewsAdapter.ItemClickListener{
+
+
+    private ProgressBar progressBar;
+    private RecyclerView recyclerView;
+    private Cursor cursor;
+    private SQLiteDatabase db;
+    private static final int NEWS_LOADER = 1;
+    private NewsAdapter newsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        progressBar=(ProgressBar) findViewById(R.id.progressBar);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirst = prefs.getBoolean("isfirst", true);
+
+        if (isFirst) {
+            initialload();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("isfirst", false);
+            editor.commit();
+        }
+        ScheduleUtilities.scheduleRefresh(this);
+
+    }
+    //Set the adapter for showing the data in the recycler view
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+        newsAdapter = new NewsAdapter(cursor, this);
+        recyclerView.setAdapter(newsAdapter);
+    }
+
+    //initialized the method for showing the newsAPI data when the application starts
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        db.close();
+        cursor.close();
+    }
+
+    public void initialload() {
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.restartLoader(NEWS_LOADER, null, this).forceLoad();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.newser, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_refresh) {
+            initialload();
+        }
+        return true;
+    }
+
+    //
+
+    @Override
+    public Loader<Void> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<Void>(this) {
+
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+                progressBar.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public Void loadInBackground() {
+                RefreshNews.refreshNews(MainActivity.this);
+                return null;
+            }
+        };
+    }
+
+    //implemented async task loader for appropriate loader callbacks
+
+    @Override
+    public void onLoadFinished(Loader<Void> loader, Void data) {
+        progressBar.setVisibility(View.GONE);
+        db = new DBHelper(MainActivity.this).getReadableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+        newsAdapter = new NewsAdapter(cursor, this);
+        recyclerView.setAdapter(newsAdapter);
+        newsAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Void> loader) {
+    }
+
+    @Override
+    public void onItemClick(Cursor cursor, int clickedItemIndex) {
+        cursor.moveToPosition(clickedItemIndex);
+        String url = cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_URL));
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+
+}
---
+++ NewsApp/app/src/main/java/com/example/sagar/newsapp/NewsAdapter.java
@@ -0,0 +1,97 @@
+package com.example.sagar.newsapp;
+
+/**
+ * Created by sagar on 6/25/17.
+ */
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.sagar.newsapp.Data.Contract;
+import com.squareup.picasso.Picasso;
+
+
+
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+    private ItemClickListener listener;
+    private Context context;
+    private Cursor cursor;
+
+    NewsAdapter(Cursor cursor, ItemClickListener listener) {
+        this.cursor = cursor;
+        this.listener = listener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(Cursor cursor, int clickedItemIndex);
+    }
+
+    @Override
+    public NewsViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.news_rows;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        NewsViewHolder viewHolder = new NewsViewHolder(view);
+        return viewHolder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(NewsViewHolder holder, final int position) {
+        cursor.moveToPosition(position);
+
+
+        holder.newsTitle.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_TITLE)));
+        holder.newsTime.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_AUTHOR))
+                + " " + cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COMLUMN_NAME_PUBLISHED_AT)));
+        holder.newsDescription.setText(cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_NAME_DESCRIPTION)));
+
+
+        String imageURL = cursor.getString(cursor.getColumnIndex(Contract.TABLE_TODO.COLUMN_URL_TO_IMAGE));
+        Picasso.with(context).load(imageURL).into(holder.newsImageView);
+    }
+
+    //set the data to the newsadapter from the SQLLite DB
+
+
+
+    @Override
+    public int getItemCount() {
+
+        return cursor.getCount();
+    }
+
+
+    class NewsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView newsTitle;
+        TextView newsDescription;
+        TextView newsTime;
+        ImageView newsImageView;
+
+        public NewsViewHolder(View itemView) {
+            super(itemView);
+            newsTitle = (TextView) itemView.findViewById(R.id.newsTitle);
+            newsDescription = (TextView) itemView.findViewById(R.id.newsDescription);
+            newsTime = (TextView) itemView.findViewById(R.id.newsPublishedAt);
+            newsImageView= (ImageView) itemView.findViewById(R.id.newsImage);
+        }
+
+        //implemeted onclicklistener to the view holder
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            listener.onItemClick(cursor, position);
+        }
+    }
+
+}
